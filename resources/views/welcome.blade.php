e85d784dd4ebcb601c3993dcbfe76a86a4a5db1b
Mike Brain <mikeybrain1302@gmail.com>
2017-08-17 13:45:51 +0100

    Social media links and "coming soon"
    Added the required social media links and also the "website coming soon..."

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Raleway', sans-serif;
+                font-family: "Franklin Gothic Medium", "Franklin Gothic", "ITC Franklin Gothic", Arial;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
@@ -35,25 +35,37 @@
                 position: relative;
             }
 
-            .top-right {
+            .bottom-social {
                 position: absolute;
-                right: 10px;
-                top: 18px;
+                bottom: 20px;
+            }
+
+            .footer {
+                position: absolute;
+                bottom: 5px;
+                font-size: 10px;
             }
 
             .content {
                 text-align: center;
             }
 
-            .title {
-                font-size: 84px;
+            .developed {
+                font-size: 26px;
+                font-weight: bold;
+            }
+
+            .links {
+                float: left;
+                display: flex;
+                height: 45px;
+                width: 100px;
             }
 
             .links > a {
                 color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
+                font-size: 14px;
+                font-weight: bold;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
@@ -62,33 +74,47 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .socailLink {
+                height: 100%;
+                width: 100%;
+            }
+
+            .socialLogo {
+                height: 75%;
+                width: 75%;
+            }
+
+            .socialLogo:hover {
+                height: 80%;
+                width: 80%;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <img src="../images/logo.png">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="developed m-b-md">
+                <br>
+                <br>
+                    Website coming soon...
                 </div>
+            </div>
+
+            <div class="bottom-social links">
+                <div class="socialLink">
+                    <a href="https://www.facebook.com/Capone-Clothing-1721731367854219/" target="_blank"> <img class="socialLogo" src="../images/Socials/facebook.png"> </a>
+                </div>
+                <div class="socialLink">
+                    <a href="https://www.instagram.com/caponeclothing/" target="_blank"> <img class="socialLogo" src="../images/Socials/instagram.png"> </a>
+                </div>
+            </div>
+
+            <div class="footer">
+                Created by: LBevan & MBrain of 2BDeveloped ltd
             </div>
         </div>
     </body>
