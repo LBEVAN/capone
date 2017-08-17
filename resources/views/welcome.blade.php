d9e0f8590e47687d38366792fdd6299bcd682cb9
Unknown <LukeRJBevan@gmail.com>
2017-08-17 15:00:40 +0100

    Move images to public folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,111 +5,33 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Capone Clothing</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: "Franklin Gothic Medium", "Franklin Gothic", "ITC Franklin Gothic", Arial;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .bottom-social {
-                position: absolute;
-                bottom: 20px;
-            }
-
-            .footer {
-                position: absolute;
-                bottom: 5px;
-                font-size: 10px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .developed {
-                font-size: 26px;
-                font-weight: bold;
-            }
-
-            .links {
-                float: left;
-                display: flex;
-                height: 45px;
-                width: 100px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                font-size: 14px;
-                font-weight: bold;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .socailLink {
-                height: 100%;
-                width: 100%;
-            }
-
-            .socialLogo {
-                height: 75%;
-                width: 75%;
-            }
-
-            .socialLogo:hover {
-                height: 80%;
-                width: 80%;
-            }
-        </style>
+        <link href="css/welcome.css" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
 
             <div class="content">
-                <img src="..\images\logo.png">
-
+                <div class="company m-b-md">
+                    CAPONE CLOTHING
+                </div>
+                
                 <div class="developed m-b-md">
-                <br>
-                <br>
-                    Website coming soon...
+                    COMING SOON
                 </div>
             </div>
 
             <div class="bottom-social links">
                 <div class="socialLink">
-                    <a href="https://www.facebook.com/Capone-Clothing-1721731367854219/" target="_blank"> <img class="socialLogo" src="..\images\Socials\facebook.png"> </a>
+                    <a href="https://www.facebook.com/Capone-Clothing-1721731367854219/" target="_blank"> 
+                        <img class="socialLogo" src="images/social/facebook.png"> 
+                    </a>
                 </div>
                 <div class="socialLink">
-                    <a href="https://www.instagram.com/caponeclothing/" target="_blank"> <img class="socialLogo" src="..\images\Socials\instagram.png"> </a>
+                    <a href="https://www.instagram.com/caponeclothing/" target="_blank"> 
+                        <img class="socialLogo" src="images/social/instagram.png">
+                    </a>
                 </div>
             </div>
 
