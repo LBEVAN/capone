1bcf4a075ec0ebb4d6ec9629bfb52fcc25d129ef
LBEVAN <LukeRJBevan@gmail.com>
2017-09-05 10:27:26 +0100

    Initial product page

--- resources/views/store/show.blade.php
+++ resources/views/store/show.blade.php
@@ -2,19 +2,38 @@
 
 @section('head')
     @parent
-    <link href="{{ asset('css/store.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/product.css') }}" rel="stylesheet">
 @stop
 
 @section('content')
-    <div class="page-header">{{ $product->name }}</div>
+    <form method="POST">
+        <div class="product-container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="{{ '../' . $product->imagePath }}">
+                </div>
 
-    <div class="col-md-4">
-        <img src="{{ '../' . $product->imagePath }}">
-    </div>
-
-    <div class="col-md-8">
-        test
-    </div>
-
-   
+                <div class="col-md-8">
+                    <div class="product-title">{{ $product->name }}</div>
+                    <div class="product-description">
+                        @foreach(explode(',', $product->description) as $descLine)
+                            <p>{{ $descLine }}</p>
+                        @endforeach
+                    </div>
+                    <div>
+                        <div class="col-md-6">
+                            <select id="size" name="size" class="form-control" required="required">
+                                @foreach($product->productStock as $productStock)
+                                    <option value="{{ $productStock->size->code }}">{{ $productStock->size->description }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <button class="btn btn-primary btn-block">Add to Basket</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 @stop
