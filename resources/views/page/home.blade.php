c2abda7e134a3cf845c2e7b51ebae62ec65ccd96
Mike Brain <mikeybrain1302@gmail.com>
2017-09-07 00:26:35 +0100

    Home Screen
    Completed the home screen (mainly layout and carousel). This will need to be finalised

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -6,23 +6,112 @@
 @stop
 
 @section('content')
-    <div class="home-header">
+    <div class="home-header">        
         <div class="home-header-heavy">CAPONE</div>
         <div class="home-header-light">CLOTHING</div>
     </div>
 
-    <div class="row" style="margin-top: 20px;">
-        <div class="hidden-xs col-sm-4 col-lg-4">
+    <!-- Carousel Starts -->
+    <div class="row" style="margin-top: 30px;">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img id="caponeCarousel" class="first-slide" src="images/caponeHeader1.png" alt="First slide">
+                </div>
+                <div class="item">
+                    <img id="caponeCarousel" class="second-slide" src="images/caponeHeader3.png" alt="Second slide">
+                </div>
+                <div class="item">
+                    <img id="caponeCarousel" class="third-slide" src="images/caponeHeader2.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <!-- Carousel Ends -->
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-5">
+            <h2 class="featurette-heading">Swansea's Next Fitness Model</h2>
+            <p class="lead">
+                This year we will be sponsoring the Swansea Next Fitness Model.
+                We'll be giving prizes to 1st, 2nd and 3rd places and there will also be a stall set up on the day.
+            </p>
+            <p class="lead">
+                This is a rewarding event for everyone involved, 
+                from bringing the community together in such a positive way to starting careers in the fitness industry.
+            </p>
+            <p class="lead"> 
+                Come join us in Swansea at The Penyrheol Theatre on Saturday 30th September - We can't wait to see you there! 
+            </p>
+        </div>
+        <div class="col-md-5 col-md-offset-2">
+            <div class="panel">
+                <img src="images/caponeSwanseaNTM.png" class="panel-avatar">
+            </div>
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-5 col-md-push-5">
+            <h2 class="featurette-heading">Capone Clothing</h2>
+            <p class="lead"> 
+                Capone Clothing boasts a range of comfortable and quality street wear, without the steep price tag            
+            </p>
+            <p class="lead"> 
+                Capone Clothing isn't about cheap materials and massive profits, we're about spreading an 
+                ambitious vibe and creating the Capone Family.
+            </p>
+            <p class="lead"> 
+                Make sure you get in touch using...
+            <br/>
+                <text style="font-size: 22px; font-weight:700;"> #CAPONECLOTHING</text>
+            </p>
+        </div>
+        <div class="col-md-5 col-md-pull-5">
+            <div class="panel">
+                <img src="images/caponeHome.png" class="panel-avatar">
+            </div>
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+
+    <!-- 
+        
+        USE THIS FOR PROMOTIONAL ITEMS??? 
+
+    -->
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-lg-4">
             <figure class="profile">
                 <img src="images/caponeHome2.png" class="profile-avatar" alt="profile-avatar">
             </figure>
         </div>
-        <div class="col-xs-6 col-sm-4 col-lg-4">
+        <div class="col-xs-12 col-sm-12 col-lg-4">
             <figure class="profile">
                 <img src="images/caponeSwanseaNTM.png" class="profile-avatar" alt="profile-avatar">
             </figure>
         </div>
-        <div class="col-xs-6 col-sm-4 col-lg-4">
+        <div class="col-xs-12 col-sm-12 col-lg-4">
             <figure class="profile">
                 <img src="images/caponeHome.png" class="profile-avatar" alt="profile-avatar">
             </figure>
