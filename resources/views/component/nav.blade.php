ef6160a19729bd983c40ba3232dcbd16cb748601
Mike Brain <mikeybrain1302@gmail.com>
2017-09-09 01:50:52 +0100

    Naviagtion Resizing
    Navigation resizes with the cart Icon for every browser

--- resources/views/component/nav.blade.php
+++ resources/views/component/nav.blade.php
@@ -1,31 +1,30 @@
 @section('nav') 
-    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: white;">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ route('/') }}">Capone Clothing</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ route('store.index') }}">Store</a></li>
-                    <li><a href="{{ route('socials') }}">Social Media</a></li>
-                    <li><a href="{{ route('contact') }}">Contact</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a class="fa fa-shopping-cart fa-lg dropdown-toggle" id="cart" data-toggle="dropdown"></a>
-                            @component('component.cart', ['numToShow' => 5])@endcomponent
-                        </li>
-                    <!-- <li><a href="#">Test1</a></li> -->
-                    <!-- <li><a href="#">Test2</a></li>  -->
-                    <!-- use the following on an <li> tag for active screen: class="active" style="padding-right:15px;" -->
-                </ul>
-            </div>
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color: white;">
+    <div class="container">
+        <div class="navbar-header" style="float: left;">
+            <button type="button" class="navbar-toggle collapsed" style="float: left; margin-left: 5px;" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ route('/') }}">Capone Clothing</a>
         </div>
-    </nav>
+        <div style="float: right; margin-left: 5px; margin-right: 5px;">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="fa fa-shopping-cart fa-lg dropdown-toggle" id="cart" data-toggle="dropdown"></a>
+                    @component('component.cart', ['numToShow' => 5])@endcomponent
+                </li>
+            </ul>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse navbar-left">
+            <ul class="nav navbar-nav">
+                <li><a href="{{ route('store.index') }}">Store</a></li>
+                <li><a href="{{ route('socials') }}">Social Media</a></li>
+                <li><a href="{{ route('contact') }}">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 @show
